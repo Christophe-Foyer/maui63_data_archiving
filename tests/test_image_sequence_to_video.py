3924ca2ee10bec31d9cb102dcb9a87344f3c8152
Christophe Foyer <christophe@cfoyer.com>
2025-10-06 12:17:30 +0100

    seems to pass tests when not using arrays

--- tests/test_image_sequence_to_video.py
+++ tests/test_image_sequence_to_video.py
@@ -54,20 +54,16 @@ def make_image_dir(num_img=20):
 @pytest.mark.parametrize("converter_type", ["h265", "jxl"])
 def test_images_to_lossless(converter_type):
     with make_image_dir() as dir, tempfile.TemporaryDirectory() as f:
-        images = [os.path.join(dir, file) for file in os.listdir(dir)]
-
         output_path = os.path.join(f, "output.mkv")
 
-        convert_images(images, output_path, converter_type=converter_type, check_frames=True)
+        convert_images(dir, output_path, converter_type=converter_type, check_frames=True)
 
 
 @pytest.mark.parametrize("converter_type", ["h265", "jxl"])
 def test_images_to_lossless_smaller(converter_type):
     with make_image_dir() as dir, tempfile.TemporaryDirectory() as f:
-        images = [os.path.join(dir, file) for file in os.listdir(dir)]
-
         output_path = os.path.join(f, "output.mkv")
         
-        convert_images(images, output_path, converter_type=converter_type, check_frames=False)
+        convert_images(dir, output_path, converter_type=converter_type, check_frames=False)
 
         assert du(dir) > du(f), f"Output was larger than the input ({du(dir)} vs {du(f)})"
